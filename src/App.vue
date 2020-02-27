5e8ae98378183470a4c779f790c571b49557b0f8
Steven Cymber <stevencymber@gnail.com>
2020-02-27 11:52:08 +0000

    initial brewdog committed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <beer-select :beers='beers'></beer-select>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AllBeers from './components/AllBeers.vue';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      beers: [],
+      selectedBeer: null
+    }
+  },
+
+  mounted(){
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(res => res.json())
+    .then(data => this.beers = data)
+  },
+
   components: {
-    HelloWorld
+"beer-select": AllBeers,
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/AllBeers.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+<div>
+<ul>
+<li v-for="(beer, index) in beers" :beer="beer" :key="index">{{beer.name}}</li>
+</ul>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'beer-select',
+  props: ['beers'],
+
+}
+
+</script>
+
+<style lang="css" scoped>
+</style>
